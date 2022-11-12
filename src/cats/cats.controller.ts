8d820215415608c897d924c43bc346964866c77e
ParkjunHyukkk <wnsgur5232@naver.com>
2022-11-12 21:18:09 +0900

    2022-11-12 nestJsArchitecture_01

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Delete, Put, Patch } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  CreateCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return ' update ';
+  }
+
+  @Delete('id')
+  deleteCat() {
+    return ' delete cat ';
+  }
+}
